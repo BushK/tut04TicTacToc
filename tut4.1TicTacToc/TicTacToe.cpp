7f1f158350f38f4a098f42acc2d1d10bcb78415a
BushK <kmabusha2@GMAIL.COM>
2016-03-16 12:54:24 +0200

    defining functions of the game

--- tut4.1TicTacToc/TicTacToe.cpp
+++ tut4.1TicTacToc/TicTacToe.cpp
@@ -1,2 +1,35 @@
 #include "TicTacToe.h"
 
+#include "TicTacToe.h"
+#include <iostream>
+using namespace std;
+
+TicTacToe::TicTacToe()
+{
+	board = "---------";
+	player[0] = 'X';
+	player[1] = 'O';
+	score[0] = 0;
+	score[1] = 0;
+	draw = 0;
+}
+
+void TicTacToe::restart()
+{
+	board = "---------";
+}
+
+bool TicTacToe::move(int choice, char player)
+{
+	bool play;
+	if (board[choice - 1] != '-')
+	{
+		play = false;
+	}
+	else
+	{
+		board[choice - 1] = player;
+		play = true;
+	}
+	return play;
+}
